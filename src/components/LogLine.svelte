7b4fd40378509d8490dba75401c655dbbb94c392
Thierry Poinot <thierry@shoot-shoot.com>
2022-09-03 10:35:11 +0200

    feat: allow edition of items in a modal

--- src/components/LogLine.svelte
+++ src/components/LogLine.svelte
@@ -13,6 +13,30 @@
     {:else}
         <span class="summary">{summary}</span>
     {/if}
+
+    <input type="checkbox" id={`item-edit-line-${uuid}`} class="modal-toggle" bind:checked={editable} on:change={() => dispatch('edit', {uuid, editable: false})} />
+    {#if editable}
+        <label for={`item-edit-line-${uuid}`} class="modal cursor-pointer">
+            <label class="modal-box relative" for="">
+                <h3 class="text-lg font-bold">Edit item #{uuid}</h3>
+                <hr/>
+                <form on:submit|preventDefault={() => dispatch('edit', {summary: editSummary, editable: false, uuid, date: editDate})}>
+                    <div class="form-group flex items-start justify-around">
+                        <label for="edit-summary-{uuid}" class="label w-1/2">
+                            <span class="label-text">Summary</span>
+                        </label>
+                        <textarea autofocus id="edit-summary-{uuid}" cols="30" rows="10" class="textarea textarea-bordered" bind:value={editSummary}></textarea>
+                    </div>
+                    <div class="form-group flex">
+                        <label for="edit-date-{uuid}" class="label w-1/2">Date</label>
+                        <input type="datetime-local" class="input input-bordered" id="edit-date-{uuid}" bind:value={editDate}>
+                    </div>
+                    <button class="btn">Save</button>
+                </form>
+            </label>
+        </label>
+
+    {/if}
     <div class="absolute inset-y-0 right-0">
 
         {#if tags.length > 0}
@@ -29,11 +53,20 @@ import dayjs from '../libs/dayjs.js';
 import {createEventDispatcher} from "svelte";
 import {parseTags} from '../libs/stores.js';
 export let date = null;
+export let editable = false;
 export let summary = '';
-export let details;
-export let type;
+export let details = undefined;
+export let type = undefined;
 export let endDate = undefined;
 export let uuid;
+export let editSummary = '';
+export let editDate = '';
+const dispatch = createEventDispatcher();
+
+$: if (editable && editSummary === '') {
+    editSummary = summary;
+    editDate = date;
+}
 
 const debug = () => {
     alert(JSON.stringify($$props, null, '  '))
