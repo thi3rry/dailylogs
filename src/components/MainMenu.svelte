99f3de46298351f6dc9031c6148f2511b316413c
Thierry Poinot <thierry@shoot-shoot.com>
2022-09-03 11:20:21 +0200

    chore: create an help page

--- src/components/MainMenu.svelte
+++ src/components/MainMenu.svelte
@@ -1,4 +1,5 @@
 <ul class={$$props.class} tabindex={$$props.tabindex}>
     <li><a href="/">Liste</a></li>
     <li><a href="/tags">Tags</a></li>
+    <li><a href="/help">Help</a></li>
 </ul>
---
+++ src/routes/help/+page.svelte
@@ -0,0 +1,17 @@
+<div class="prose">
+
+    <h1>Help</h1>
+
+    This app is for logging message at the date your are submitted.
+
+    <br>
+    <p>
+        You can add tags in yours messages by prefixing words with a hash.
+        <br>
+        Tags create automatically.
+    </p>
+
+    <p>
+        All data are store in your browser in localStorage.
+    </p>
+</div>
